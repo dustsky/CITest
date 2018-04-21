37c7d94f6d4fce861491b7d327b41b095a01e4c8
dustsky <gsmxsky@gmail.com>
2018-04-21 23:12:37 +0800

    init pro

---
+++ CITest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CITest
+//
+//  Created by DUSTSKY on 21/04/2018.
+//  Copyright © 2018 DUSTSKY. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
